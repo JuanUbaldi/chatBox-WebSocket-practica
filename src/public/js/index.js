826e243cc1747bd4a2994d87fbb27f512abda52d
JuanUbaldi <106642655+JuanUbaldi@users.noreply.github.com>
2023-06-27 08:00:58 -0300

    commit

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -3,18 +3,25 @@ const socket = io();
 //codigo del front
 
 let nombreUsuario = "";
+let gender = "";
 async function pedirNombre() {
   const { value: formValues } = await Swal.fire({
-    title: "Coloca tu nombre",
-    html: '<input id="swal-input1" class="swal2-input">',
+    title: "coloca tu nombre y tu genero",
+    html:
+      '<input id="swal-input1" placeholder="nombre" class="swal2-input">' +
+      '<input id="swal-input2" placeholder="genero (hombre, mujer, otro)"  class="swal2-input">',
     focusConfirm: false,
     preConfirm: () => {
-      return [document.getElementById("swal-input1").value];
+      return [
+        document.getElementById("swal-input1").value,
+        document.getElementById("swal-input2").value,
+      ];
     },
   });
 
   if (formValues) {
-    nombreUsuario = formValues;
+    nombreUsuario = formValues[0];
+    gender = formValues[1];
   }
 }
 pedirNombre();
@@ -25,6 +32,7 @@ chatBox.addEventListener("keyup", ({ key }) => {
     socket.emit("FrontToBack", {
       user: nombreUsuario,
       msg: chatBox.value,
+      genero: gender,
     });
     chatBox.value = "";
   }
@@ -33,10 +41,26 @@ chatBox.addEventListener("keyup", ({ key }) => {
 socket.on("BackToFront", (msgs) => {
   let msgsFormateados = "";
   msgs.forEach((msg) => {
-    msgsFormateados += `  <div style='border: 1px solid violet'>
+    if (msg.genero === "hombre") {
+      msgsFormateados += `  <div class="clase1" >
  <p> ${msg.user} </p>
+ 
 <p> ${msg.msg}</p> </div>
 `;
+    } else if (msg.genero === "mujer") {
+      msgsFormateados += `  <div class="clase2" >
+ <p> ${msg.user} </p>
+ 
+<p> ${msg.msg}</p> </div>
+`;
+    } else if (msg.genero === "otro") {
+      msgsFormateados += `  <div class="clase3" >
+ <p> ${msg.user} </p>
+ 
+<p> ${msg.msg}</p> </div>
+`;
+    }
+
     const divMsgs = document.getElementById("div-msgs");
     divMsgs.innerHTML = msgsFormateados;
   });
